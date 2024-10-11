8614abdbb0aeb29b016b2bd0e91f4e1fa5cf75c5
songcom <nmsong@raresoft.co.kr>
2024-10-11 11:20:34 +0900

    1111111

--- git-test/src/test.java
+++ git-test/src/test.java
@@ -4,6 +4,10 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
 	}
 
 }
